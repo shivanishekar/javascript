089a88170d8d8c92a6e9c6c31321746453775350
shivanishekar <shivagamishivani06@gmail.com>
2024-11-19 16:55:37 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -17,8 +17,48 @@
 //""=>false
 //"hitesh"=>true
 
-let someNumber=33;
-let StringNumber=String(someNumber);
-console.log(StringNumber);
-console.log(typeof StringNumber);
+// let someNumber=33;
+// let StringNumber=String(someNumber);
+// console.log(StringNumber);
+// console.log(typeof StringNumber);
+
+// /**********Conversions**********/
+// let value=3;
+// let negvalue=-value;
+//console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+// let str1="hello"
+// let str2=" hitesh"
+// let str3=str1+str2;
+
+// console.log(str3);
+// console.log("1"+1);
+// console.log(1+"2");
+// console.log(1+2+"3");
+// console.log("30"+1+2);
+
+// console.log(+true);
+// let gamecounter=100;
+// gamecounter++;
+// console.log(gamecounter);
+
+// let x=3;
+// let y=x++;//x:4;y:3
+// console.log(x,y);//4 3
+
+
+
+
+
+
+
+
+
 
