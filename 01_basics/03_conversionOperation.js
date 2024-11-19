7ced74e241a13f7b5ecc6c1fde763e1132459d4c
shivanishekar <shivagamishivani06@gmail.com>
2024-11-19 16:02:20 +0000

    conversions

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+// let score="33abc";
+
+
+// console.log(typeof score);
+// let valueInNumber=Number (score)
+// console.log(typeof valueInNumber);
+
+//"33"=>33
+//"33abc"=>NAN
+//true=>1;false=>0
+
+// let isLoggedIn="hitesh"
+// let IsLoggedIn=Boolean(isLoggedIn);
+// console.log(IsLoggedIn);
+
+//1=>true; 0=>false
+//""=>false
+//"hitesh"=>true
+
+let someNumber=33;
+let StringNumber=String(someNumber);
+console.log(StringNumber);
+console.log(typeof StringNumber);
+
