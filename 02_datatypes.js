a7aaad3c3ee31e5eec8d000731a5d915dced7581
shivanishekar <shivagamishivani06@gmail.com>
2024-11-19 15:36:24 +0000

    container config

---
+++ 02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict";//treat all js code as newer version
+
+//alert(3+3)//we are using nodejs,not browser
+console.log(3 +3);//code readability
+
+console.log("Hitesh");
+
+let name="shivani";
+let age=22;
+let isloggedIn=false;
+
+//number=>2 to power 53
+//bigint
+//string=>""
+//boolean=>true / false
+//null=>standalone value //empty
+//undefined=> value not assigned 
+//symbol=> unique 
+
+//object=>
+    console.log(typeof undefined);//undefined
+    console.log(typeof null);//object
+    
+    
